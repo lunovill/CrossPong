d0b5d4c0d4b73066d234a9c92ba29d13d0d3e3f5
Adnan BOUDJELAL <aboudjel@bess-f2r2s14.clusters.42paris.fr>
2023-11-06 00:07:55 +0100

    adding generic gitignore

--- node_modules/three-custom-shader-material/dist/three-custom-shader-material.cjs.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./declarations/src/index";
-export { default } from "./declarations/src/index";
-//# sourceMappingURL=three-custom-shader-material.cjs.d.ts.map
--- node_modules/three-custom-shader-material/vanilla/dist/three-custom-shader-material-vanilla.cjs.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "../../dist/declarations/src/vanilla";
-export { default } from "../../dist/declarations/src/vanilla";
-//# sourceMappingURL=three-custom-shader-material-vanilla.cjs.d.ts.map
